20e0272c2a3f587a91557e5b01dd8e782a1455af
Oliver Abreu <oliverabreu@icloud.com>
2019-08-06 23:51:34 -0400

    This commit should show solution for 00

--- src/00_hello.py
+++ src/00_hello.py
@@ -1,1 +1,9 @@
 # Print "Hello, world!" to your terminal
+
+# Print "Hello, world!" to your terminal in an unncessary way:
+greeting = "Hello"
+punctuation_one = ","
+planet = "world"
+punctuation_two = "!"
+
+print(f'{greeting}{punctuation_one} {planet}{punctuation_two}')
